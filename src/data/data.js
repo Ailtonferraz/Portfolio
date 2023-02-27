eea4330c8f187eb7ce133f03d8d189f5de445f07
AiltonFerraz <ailtonferraz56@gmail.com>
2023-02-27 09:27:38 +0000

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+import WorkIm from '../assets/projects/chess.png'
+import RealEst from '../assets/projects/chess.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Java",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+     
+
+]
